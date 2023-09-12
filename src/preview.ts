6c86783680f6d72d029e441aea580d3780f1e266
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2023-09-12 11:27:17 +0200

    set the urls

--- src/preview.ts
+++ src/preview.ts
@@ -18,7 +18,7 @@ import { PARAM_KEY } from "./constants";
 
 const preview: ProjectAnnotations<Renderer> = {
   parameters: {
-    [PARAM_KEY.REPO]: `https://github.com/stackblitz/docs`,
+    repositoryUrl: 'https://github.com/sulco/viteconf-storybook-demo',
   }
 };
 
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -2,33 +2,29 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Button } from "./Button";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Button> = {
   title: "Example/Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
   tags: ["autodocs"],
   parameters: {
-    // repositoryUrl: `https://github.com/stackblitz/docs`,
+    repositoryUrl: `https://github.com/sulco/viteconf-storybook-demo`,
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Primary: Story = {
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
   args: {
     primary: true,
     label: "Button",
   },
   parameters: {
-    // repositoryUrl: `https://github.com/stackblitz/docs`
-    filePath: 'docs/links.ts'
+    repositoryUrl: 'https://github.com/sulco/viteconf-storybook-demo',
+    filePath: 'src/stories/Button.jsx'
   }
 };
 
